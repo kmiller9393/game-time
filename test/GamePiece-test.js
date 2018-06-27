f7cdc0bc1b80333a65dd64303228aaa2eb52965d
Kurt Miller <kmiller9393@gmail.com>
2018-06-27 16:25:58 -0600

    Add createApple function to add apple to canvas on user click

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
 const GamePiece = require('../lib/GamePiece.js');
+
 describe('GamePiece', function() {
 
   it('should have properties', function() {
@@ -38,7 +39,7 @@ describe('GamePiece', function() {
     const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
     
     gamePiece1.move();
-    
+
     assert.equal(gamePiece1.x, 50.5);
   })
 })
