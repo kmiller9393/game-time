f428ed2a43d16467f2320db5244e526dc57ee9f2
Kurt Miller <kmiller9393@gmail.com>
2018-07-08 17:42:06 -0600

    Update test for moveSnake function

--- test/Snake-test.js
+++ test/Snake-test.js
@@ -5,9 +5,9 @@ const Apple = require('../lib/Apple.js')
 describe('Snake', function() {
 
 it('should instantiate a new snake with 5 body parts', function() {
- let snake = new Snake()
- snake.createSnake();
- assert.deepEqual(snake.body.length, 5)
+ let snake = new Snake();
+ 
+ assert.equal(snake.body.length, 5);
 })
 
 it('should be able grow when eats apple', function() {
@@ -27,11 +27,15 @@ it('should be able grow when eats apple', function() {
 
 it('should be able to move', function() {
  const newSnake = new Snake(0, 0);
- newSnake.createSnake();
- newSnake.tail = newSnake.body.shift();
+
+ newSnake.tail = newSnake.body[0]
  newSnake.head = newSnake.body[newSnake.body.length - 1];
 
- assert.deepEqual(newSnake.head.x, 80);
+ newSnake.moveSnake();
+ newSnake.direction = 'right';
+
+ assert.equal(newSnake.head.x, 80);
+ assert.equal(newSnake.tail.x, 100);
 })
 
 it('should die when it collides into itself', function() {
@@ -50,11 +54,14 @@ it('should die when it collides into itself', function() {
   assert.equal(newSnake.isGameOver, true);
   })
 
-it.skip('should die when collides into wall', function(){
+it.only('should die when collides into wall', function() {
   const newSnake = new Snake(0, 0);
   newSnake.createSnake();
-  newSnake.head = newSnake.body[this.body.length - 1];
-
+  newSnake.moveSnake();
+  newSnake.direction = 'up';
+  newSnake.collideIntoWall();
+  newSnake.head.y === 500;
+  assert.equal(newSnake.isGameOver, true);
  
-  newSnake  })
+  })
 })
